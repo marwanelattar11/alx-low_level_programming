25027841d6229c8a3af48b463dde1711763457cf
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-15 12:43:17 +0000

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - entery poinr
+ *
+ * Description: A C program using another library
+ *
+ * Return: 1 (not success)
+ *
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
