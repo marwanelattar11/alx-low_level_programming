4573a6409c020a40eb6b24d761bdf5a64794abb1
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-16 13:53:05 +0000

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: a
+ *
+ * Return: always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
