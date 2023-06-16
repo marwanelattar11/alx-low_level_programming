9bbb9017c14f5073fc6e843021718094913eddd8
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-16 12:40:53 +0000

    a

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: grater than or smaller than
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("is %d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("is %d and is 0\n", n);
+	}
+	else
+	{
+		printf("is %d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
