e0acb0f2232fe2ef6a13916c3eadd1747e6dfbb3
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-16 15:08:47 +0000

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: a
+ *
+ * Return: always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for(j = 1; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar (48 + i);
+				putchar (48 + j);
+				putchar (',');
+				putchar (' ');
+			}	
+		}
+	}
+	putchar ('\n');
+	
+	return (0);
+}
