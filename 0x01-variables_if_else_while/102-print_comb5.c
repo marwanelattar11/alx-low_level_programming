71d1fddfdbb82343b4ed06d4231675a26b03b761
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-17 01:25:23 +0000

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+/**
+ * main - entery point
+ *
+ * Desctription: a
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int firstdigit = 0, secondigit;
+
+	while (firstdigit <= 99)
+	{
+		secondigit = firstdigit;
+		while (secondigit <= 99)
+		{
+			if (secondigit != firstdigit)
+			{
+				putchar ((firstdigit / 10) + 48);
+				putchar ((firstdigit % 10) + 48);
+				putchar (' ');
+				putchar ((secondigit / 10) + 48);
+				putchar ((secondigit % 10) + 48);
+				if (firstdigit != 98 || secondigit != 99)
+				{
+					putchar ('.');
+					putchar (' ');
+				}
+			}
+			secondigit++;
+		}
+		firstdigit++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
