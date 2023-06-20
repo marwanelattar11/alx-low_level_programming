4a4a117beb19dff7b6dfea5fd2e30eed51d7f8b6
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-20 14:41:26 +0000

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - entery point
+ *		the alphabet a-z
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
