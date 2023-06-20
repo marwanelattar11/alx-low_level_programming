f6e58054a0209e5f337a2af1a2314e85e927ff1f
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-20 14:00:54 +0000

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entery point
+ *
+ * Description: a
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(x[i]);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the char
+ * @c: the char to print
+ * Return: on success 1
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c ,1));
+}
