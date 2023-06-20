17129a796c2a26977625ba9c4af79b415fd8c6c4
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-20 22:00:10 +0000

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - entry
+ *
+ * @x: first input
+ * @y: second
+ *
+ * Return: sum
+*/
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
