385dcedea6f77d7eab8fc12e24179a45be4e0417
marwanelattar11 <marwanel3ttar@gmail.com>
2023-06-22 12:48:42 +0000

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Entry
+ * @n: input
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar ('_');
+		_putchar ('\n');
+	}
+}
