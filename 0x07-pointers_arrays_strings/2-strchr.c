de589f39a4d336244bf0e18ab3722fe94d3013d4
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-03 19:25:54 +0000

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s)
+	for (i = 0, str[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-		else
-			i++;
 	}
 	return ('\0');
 }
