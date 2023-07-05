157390ada2e77656e21e45f4bc3a64cdbaa68696
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-05 09:35:42 +0000

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
-/***/
+/**
+ * _strlen_recursion - Entry
+ * @s: input
+ * Return: sum;
+*/
 
 int _strlen_recursion(char *s)
 {
-	static int sum = 0;
+	int sum = 0;
 
 	if (*s != '\0')
 	{
-		sum += 1;
-		_strlen_recursion(s + 1);
+		sum += _strlen_recursion(s + 1) + 1;
 	}
 	return (sum);
 }
