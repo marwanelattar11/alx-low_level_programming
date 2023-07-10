df936c427a3022665fa1903dbe9a51053bd30bd5
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-10 10:30:40 +0000

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	{
 		return  (NULL);
 	}
-	ptr = malloc(height * sizeof(int));
+	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		return (NULL);
