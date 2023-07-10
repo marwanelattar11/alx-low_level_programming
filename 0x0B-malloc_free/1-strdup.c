00bab68921badabc3e56e966d4fa0f732ed5e71d
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-10 08:37:54 +0000

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Entry
+ * @str: input
+ * Return: ptr
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size]; size++)
+	;
+
+	ptr = malloc(size * sizeof(*str) + 1);
+
+	if (ptr != 0)
+		for (; i < size; i++)
+			ptr[i] = str[i];
+	else
+		return (NULL);
+	return (ptr);
+}
