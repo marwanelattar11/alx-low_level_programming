2da5d3588a3dadbe0850255975f501b4247344f9
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-17 09:31:33 +0000

    a

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - entry
+ * @d: pointer
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
